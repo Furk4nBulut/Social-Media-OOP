2e96a776b06ed5a5e221f2e21eb5aeec1f94493e
Furkan Bulut <furkanbtng@gmail.com>
2023-03-23 01:31:05 +0300

    Some examples added.

--- src/ooplab4/socailmedia/Main.java
+++ src/ooplab4/socailmedia/Main.java
@@ -8,13 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
         Date d = new Date();
-        User u1 = new User("aaa","aaa","aaa","aaa","aaa",d,"aaaa");
-        User u2 = new User("bbb","bbb","bbb","bbb","bbb",d,"bbb");
+        User u1 = new User("Furkan","Bulut","ToFuDuK","210316011","furkan",d,"male");
+        User u2 = new User("Ahmet","Bulut","InfinityOfDeath","ahmet","ahmet",d,"male");
         u1.addFriend(u2);
         u2.addFriend(u1);
-        u1.sendMessage("Deneme",u2);
+        u1.sendMessage("Nabıyon be ya?\n",u2);
         u2.writeInboxByIndex(0);
         u1.writeOutboxByIndex(0);
+        u2.sendMessage("Sanane\n",u1);
+        u1.writeInboxByIndex(0);
+        u2.writeOutboxByIndex(0);
     }
     
 }
